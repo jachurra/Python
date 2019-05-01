cbea40991e3cc5b7558e4203b84e708799d6ce91
Juan Achurra <42154456+jachurra@users.noreply.github.com>
2019-05-01 17:01:02 -0400

    Add files via upload

---
+++ Achurra-AWSSupportCases.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3.6
+# By Juan Achurra
+# Head of Cloud Support for Hertz Global
+# Role based all organizational dynamic accounts instance inventory 
+# Joshua: Shall we play a game?
+# If you are interested in how to dynamically read all account support cases in a single script contact me
+# juan.achurra@gmail.com
+
+# AWS ALL Account Support Cases (opened or resolved toggle)
